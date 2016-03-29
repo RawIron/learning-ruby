82e4b3429921d6972099a146a0212e262e39c131
RawIron <manianology@gmail.com>
2016-03-29 23:08:18 +0200

    new folder gotchas; use rspec instead of puts

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ruby-scratch
+## Quick Ruby hacks
+
+### gotchas
+* test language semantics
+* test short recipes
+* micro-benchmark
+
+`gotchas` are implemented as tests.
+the modules don't have a `main` function.
+to run the `gotchas` simply run the tests.
---
+++ gotchas/codeblock.rb
@@ -0,0 +1,53 @@
+# nested modules
+# anonymous functions
+# lexical scope
+# a Ruby block is not an object. The Proc class turns a block into an object.
+
+module Racing
+module Car
+	def self.shift_gear(car, gear, codeblock)
+		codeblock.call(car, gear)
+	end
+end
+end
+
+codeblock = Proc.new {|car, gear|
+  [car, gear+1]
+}
+
+RSpec.describe Racing::Car do
+  describe "#shift_gear" do
+    it "returns gear" do
+      car, gear = Racing::Car.shift_gear("audi", 4, codeblock)
+      expect(car).to eq("audi")
+      expect(gear).to eq(5)
+    end
+  end
+end
+
+
+# nested modules
+# pass module as parameter to function
+
+module Racing
+module Track
+	def self.view()
+		return "Hockenheim"
+	end
+end
+end
+
+def run(this)
+	this.view
+end
+
+
+RSpec.describe Racing::Track do
+  describe "#view" do
+    it "returns description of track" do
+      desc = run(Racing::Track)
+      expect(desc).to eq("Hockenheim")
+    end
+  end
+end
+
